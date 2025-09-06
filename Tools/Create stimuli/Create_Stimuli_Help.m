6e785c35c6e8817eb00aa80a43a10fd48b0f66d4
Ale Presacco <pressalex@hotmail.com>
2025-09-06 18:42:01 +0200

    Add files via upload

---
+++ Tools/Create stimuli/Create_Stimuli_Help.m
@@ -0,0 +1,98 @@
+function varargout = Create_Stimuli_Help(varargin)
+% CREATE_STIMULI_HELP MATLAB code for Create_Stimuli_Help.fig
+%      CREATE_STIMULI_HELP, by itself, creates a new CREATE_STIMULI_HELP or raises the existing
+%      singleton*.
+%
+%      H = CREATE_STIMULI_HELP returns the handle to a new CREATE_STIMULI_HELP or the handle to
+%      the existing singleton*.
+%
+%      CREATE_STIMULI_HELP('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in CREATE_STIMULI_HELP.M with the given input arguments.
+%
+%      CREATE_STIMULI_HELP('Property','Value',...) creates a new CREATE_STIMULI_HELP or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before Create_Stimuli_Help_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to Create_Stimuli_Help_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help Create_Stimuli_Help
+
+% Last Modified by GUIDE v2.5 13-Jan-2022 09:50:01
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @Create_Stimuli_Help_OpeningFcn, ...
+                   'gui_OutputFcn',  @Create_Stimuli_Help_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before Create_Stimuli_Help is made visible.
+function Create_Stimuli_Help_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to Create_Stimuli_Help (see VARARGIN)
+
+% Choose default command line output for Create_Stimuli_Help
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes Create_Stimuli_Help wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = Create_Stimuli_Help_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%Description of how to use the GUI to create auditory stimuli
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function Create_Stimuli_Edit_Help_Callback(hObject, eventdata, handles)
+% hObject    handle to Create_Stimuli_Edit_Help (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of Create_Stimuli_Edit_Help as text
+%        str2double(get(hObject,'String')) returns contents of Create_Stimuli_Edit_Help as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function Create_Stimuli_Edit_Help_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to Create_Stimuli_Edit_Help (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
