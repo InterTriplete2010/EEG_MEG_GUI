ae9c84f33ada1f81c1534ecd762fec36a4bd92fe
Ale Presacco <pressalex@hotmail.com>
2025-10-21 11:57:24 +0200

    Update DoDssIIR2Alex_Forward_Model_Quiet.m
    Turning off the interpreter of the title of the plots

--- Import data/MEG/TRF/DoDssIIR2Alex_Forward_Model_Quiet.m
+++ Import data/MEG/TRF/DoDssIIR2Alex_Forward_Model_Quiet.m
@@ -70,7 +70,7 @@ hold off
 xlabel('\bfTime (s)')
 ylabel('\bfAmplitude (A.U.)')
 
-title(['\bfTarget Stimulus and its Envelope of: ' wav_file_selected(1,1:end-4)])
+title(['Target Stimulus and its Envelope of: ' wav_file_selected(1,1:end-4)], 'interpreter', 'none')
 
 legend('Target Stimulus','+Envelope','-Envelope')
 
@@ -120,7 +120,7 @@ hold off
 xlabel('\bfTime (s)')
 ylabel('\bfAmplitude (A.U.)')
 
-title(['\bfBackground Noise Stimulus and its Envelope of: ' wav_file_selected_noise(1,1:end-4)])
+title(['Background Noise Stimulus and its Envelope of: ' wav_file_selected_noise(1,1:end-4)], 'interpreter', 'none')
 
 legend('Target Stimulus','+Envelope','-Envelope')
 
@@ -468,3 +468,4 @@ cd(mat_file_directory)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 end
+
